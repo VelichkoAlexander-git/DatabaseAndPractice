7d54f0ec487317cfe70f23b1fe3d4e7b5eac9dd5
Alexander Velichko <Alexander-Velichko@bk.ru>
2020-09-23 21:05:34 +0300

    careful, not completed

---
+++ Prim'sAlgorithmMaze/Prim'sAlgorithmMaze/Maze.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace Prim_sAlgorithmMaze
+{
+    public class Maze_generator
+    {
+        public const int wall = 0;
+        public const int hole = 1;
+        public const int solution = 2;
+
+        public const int vertical = 0;
+        public const int horizontal = 1;
+
+        public const int north = 0;
+        public const int south = 1;
+        public const int west = 2;
+        public const int east = 3;
+
+        List<List<int>> area;
+
+        int height;
+        int width;
+
+        public Maze_generator(int height, int width)
+        {            
+            area = new List<List<int>>();
+            if (!(height % 2 == 0) || !(width % 2 == 0))
+            {
+                throw new ArgumentException("Height and width must be odd numbers!");
+            }
+
+            this.height = height;
+            this.width = width;
+
+            area.Resize(height);
+            for (int y = 0; y < height; y++)
+            {
+                area[y].Resize(width);
+                for (int x = 0; x < width; x++)
+                {
+                    area[y][x] = wall;
+                }
+            }
+        }
+
+        public void set_cell(int y, int x, int value)
+        {
+            if ((area.Count() <= y) || (area[y].Count() <= x))
+            {
+                throw new Exception("Out of boundary!");
+            }
+            else if ((wall != value) && (hole != value))
+            {
+                throw new Exception("The input can only contain 0 or 1.");
+            }
+            else
+            {
+                area[y][x] = value;
+            }
+        }
+
+        public int get_cell(int y, int x)
+        {
+            return area[y][x];
+        }
+
+        public List<List<int>> get_maze()
+        {
+            return area;
+        }
+
+        void set_maze(List<List<int>> vect)
+        {
+            if ((vect.Count() != area.Count()) || (vect[0].Count() != area[0].Count()))
+            {
+                throw new Exception("Wrong size!");
+            }
+            else
+            {
+                for (int y = 0; y < vect.Count(); y++)
+                {
+                    for (int x = 0; x < vect[y].Count(); x++)
+                    {
+                        if ((wall != vect[y][x]) && (hole != vect[y][x]))
+                        {
+                            throw new Exception("The input can only contain 0 or 1.");
+                        }
+                        else
+                        {
+                            area[y][x] = vect[y][x];
+                        }
+                    }
+                }
+            }
+        }
+
+        void reshape(int new_height, int new_width)
+        {
+            /* Make sure, that the dimensions are odd numbers, otherwise the maze would look strange. */
+            if ((!(height % 2 == 0)) || (!(width % 2 == 0)))
+            {
+                throw new Exception("Height and width must be odd numbers!");
+            }
+
+            /* Resize. */
+            area.Resize(height);
+            for (int y = 0; y < height; y++)
+            {
+                area[y].Resize(width);
+            }
+        }
+
+        public int get_height()
+        {
+            return area.Count();
+        }
+
+        public int get_width()
+        {
+            return area[0].Count;
+        }
+    }
+
+    public static class ListExtra
+    {
+        public static void Resize<T>(this List<T> list, int sz, T c)
+        {
+            int cur = list.Count();
+            if (sz < cur)
+                list.RemoveRange(sz, cur - sz);
+            else if (sz > cur)
+            {
+                if (sz > list.Capacity)//this bit is purely an optimisation, to avoid multiple automatic capacity changes.
+                    list.Capacity = sz;
+                list.AddRange(Enumerable.Repeat(c, sz - cur));
+            }
+        }
+        public static void Resize<T>(this List<T> list, int sz) where T : new()
+        {
+            Resize(list, sz, new T());
+        }
+    }
+}
---
+++ Prim'sAlgorithmMaze/Prim'sAlgorithmMaze/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace Prim_sAlgorithmMaze
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int width = 10;
+            int height = 10;
+
+            List<List<int>> maze;
+
+            Maze_generator m_maze = new Maze_generator(height, width);
+            m_maze.set_cell(0, 1, Maze_generator.hole);
+            m_maze.set_cell(height - 1, width - 2, Maze_generator.hole);
+            Prim prim = new Prim(m_maze.get_maze());
+            prim.generate();
+            maze = m_maze.get_maze();
+
+        }
+    }
+    static class IEnumerableRandom
+    {
+        public static T RandomElement<T>(this IEnumerable<T> enumerable)
+        {
+            return RandomElementUsing<T>(enumerable, new Random());
+        }
+
+        public static T RandomElementUsing<T>(this IEnumerable<T> enumerable, Random rand)
+        {
+            int index = rand.Next(0, enumerable.Count());
+            return enumerable.ElementAt(index);
+        }
+    }
+}
