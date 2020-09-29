fcb859d121387c9f82f3e99b5887ca8829c3c6e7
Alexander Velichko <Alexander-Velichko@bk.ru>
2020-09-29 12:21:28 +0300

    Altered Lab Job

--- Lab_4/Lab_4_Var_1.sql
+++ Lab_4/Lab_4_Var_1.sql
@@ -1,4 +1,17 @@
-CREATE VIEW [dbo].ViewDoctor AS
-	SELECT FirstName, FastName, Patronymic, Specialty
-	FROM dbo.Doctor AS doct
-		INNER JOIN dbo.Specialty AS spec ON doct.Specialty_id = spec.id
+USE [ReceptionClinicDB]
+
+CREATE VIEW [dbo].ViewSpecialty
+	AS SELECT id, Specialty
+	FROM [dbo].Specialty
+
+CREATE VIEW [dbo].ViewDoctor
+	AS SELECT id, FirstName, FastName, Patronymic, DateOfBirthday, Admission—ost, FixedPercentage, Specialty_id
+	FROM [dbo].Doctor
+
+CREATE VIEW [dbo].ViewReceipt
+	AS SELECT Patient_id, Doctor_id, DateOfReceipt
+	FROM [dbo].Receipt
+
+CREATE VIEW [dbo].ViewPatient
+	AS SELECT id, FirstName, FastName, Patronymic, DateOfBirthday
+	FROM [dbo].Patient
