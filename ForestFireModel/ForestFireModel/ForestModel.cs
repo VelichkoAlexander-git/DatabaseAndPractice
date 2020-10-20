1317cfe9474a61ad07f41a6d8b04f9552c6a11d6
Alexander Velichko <Alexander-Velichko@bk.ru>
2020-10-20 14:25:58 +0300

    setting in app.config

---
+++ ForestFireModel/ForestFireModel/ForestModel.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForestFireModel
+{
+    public class ForestModel
+    {
+        public enum State : byte
+        {
+            Empty = 0,
+            Tree = 1,
+            Fire = 2
+        }
+
+        private State[,] _matrix;
+
+        public State[,] GetState()
+        {
+            return _matrix;
+        }
+
+        private static readonly Random rand = new Random();
+
+        private float _woodFireRisk { get; set; }
+
+        private float _probabilityFillingWithWood { get; set; }
+
+        public int HeightMap { get; set; }
+
+        public int WidthMap { get; set; }
+
+        protected ForestModel(float f, float p, int height, int width)
+        {
+            _woodFireRisk = f;
+            _probabilityFillingWithWood = p;
+            HeightMap = height;
+            WidthMap = width;
+
+            Initialize();
+        }
+
+        public static Result<ForestModel> Create(float f, float p, int height, int width)
+        {
+            var errors = new List<string>();
+
+            if (!(0 <= f && f <= 1)) errors.Add("Вероятность возгорания дерева только [0,1]");
+            if (!(0 <= p && p <= 1)) errors.Add("Вероятность заполнением дерева только [0,1]");
+            if (height < 1) errors.Add("Высота не может быть меньше единицы");
+            if (width < 1) errors.Add("Ширина не может быть меньше единицы");
+
+            if (errors.Any())
+            {
+                return Result<ForestModel>.Fail(errors);
+            }
+
+            return Result<ForestModel>.Success(new ForestModel(f, p, height, width));
+        }
+
+        private void Initialize()
+        {
+            _matrix = new State[HeightMap, WidthMap];
+            _matrix.Initialize();
+        }
+
+        public State[,] Step()
+        {
+            var newState = (State[,])_matrix.Clone();
+
+            int numRows = _matrix.GetLength(0);
+            int numCols = _matrix.GetLength(1);
+
+            for (int r = 1; r < numRows - 1; r++)
+            {
+                for (int c = 1; c < numCols - 1; c++)
+                {
+                    switch (_matrix[r, c])
+                    {
+                        case State.Empty:
+                            if (rand.NextDouble() < _probabilityFillingWithWood)
+                                newState[r, c] = State.Tree;
+                            break;
+
+                        case State.Tree:
+                            if (NeighborHasState(_matrix, r, c, State.Fire) || rand.NextDouble() < _woodFireRisk)
+                                newState[r, c] = State.Fire;
+                            break;
+
+                        case State.Fire:
+                            newState[r, c] = State.Empty;
+                            break;
+                    }
+                }
+            }
+
+            _matrix = newState;
+            return newState;
+        }
+
+        private bool NeighborHasState(State[,] state, int x, int y, State value)
+        {
+            for (int r = -1; r <= 1; r++)
+            {
+                for (int c = -1; c <= 1; c++)
+                {
+                    if (r == 0 && c == 0)
+                        continue;
+
+                    if (state[x + r, y + c] == value)
+                        return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
--- ForestFireModel/ForestFireModel/Program.cs
+++ ForestFireModel/ForestFireModel/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -10,19 +13,24 @@ namespace ForestFireModel
 {
     class Program
     {
-        static Timer timer;
+        public static float f;
+        public static float p;
+        public static int height;
+        public static int width;
+
+        private static Timer timer;
         private Encoding ascii = Encoding.ASCII;
         static void Main(string[] args)
         {
-            Console.OutputEncoding = System.Text.Encoding.GetEncoding(1252);
+            var parser = ParserValue();
+            if (!parser.Succeeded)
+            {
+                Console.WriteLine(String.Join(Environment.NewLine, parser.Errors));
+                Console.ReadLine();
+                return;
+            }           
 
-
-            float f = 0.7f;
-            float p = 0.8f;
-            int height = 20;
-            int width = 25;
-
-            long interval = 2000; //3 секунды
+            long interval = 2000;
 
             ForestModel model;
             var createResult = ForestModel.Create(f, p, height, width);
@@ -32,6 +40,7 @@ namespace ForestFireModel
             }
             else
             {
+                Console.OutputEncoding = System.Text.Encoding.GetEncoding(1252);
                 model = createResult.Value;
                 timer = new Timer(new TimerCallback(Drawing), model, 0, interval);
             }
@@ -39,11 +48,64 @@ namespace ForestFireModel
             Console.ReadLine();
         }
 
+        #region Parser
+        public static Result<bool> ParserValue()
+        {
+            var errors = new List<string>();
+
+            if (!IsDigitsOnlyAndPoint(ConfigurationManager.AppSettings["WoodFireRisk"])) { errors.Add("Поле WoodFireRisk не удалось преобразовать в float"); }
+            if (!IsDigitsOnlyAndPoint(ConfigurationManager.AppSettings["FillingWithWood"])) { errors.Add("Поле FillingWithWood  не удалось преобразовать в float"); }
+            if (!IsDigitsOnly(ConfigurationManager.AppSettings["HeightMap"])) { errors.Add("Поле HeightMap не удалось преобразовать в int"); }
+            if (!IsDigitsOnly(ConfigurationManager.AppSettings["WidthMap"])) { errors.Add("Поле WidthMap не удалось преобразовать в int"); }
+
+            if (errors.Any())
+            {
+                return Result<bool>.Fail(errors);
+            }
+
+            CultureInfo ci = (CultureInfo)CultureInfo.CurrentCulture.Clone();
+            ci.NumberFormat.CurrencyDecimalSeparator = ".";
+
+            f = float.Parse(ConfigurationManager.AppSettings["WoodFireRisk"], NumberStyles.Any, ci);
+            p = float.Parse(ConfigurationManager.AppSettings["FillingWithWood"], NumberStyles.Any, ci);
+            height = int.Parse(ConfigurationManager.AppSettings["HeightMap"]);
+            width = int.Parse(ConfigurationManager.AppSettings["WidthMap"]);
+
+            return Result<bool>.Success(true);
+        }
+
+        private static bool IsDigitsOnly(string str)
+        {
+            foreach (char c in str)
+            {
+                if (c < '0' || c > '9')
+                    return false;
+            }
+
+            return true;
+        }
+
+        private static bool IsDigitsOnlyAndPoint(string str)
+        {
+            if (str.Where(c => c == '.').Count() > 1) return false;
+            else
+                str = str.Replace(".", "");
+
+            foreach (char c in str)
+            {
+                if (c < '0' || c > '9')
+                    return false;
+            }
+
+            return true;
+        }
+        #endregion
+
         public static void Drawing(Object model)
         {
             ForestModel forest = (ForestModel)model;
             Console.Clear();
-            
+
             for (int y = 0; y < forest.HeightMap; y++)
             {
                 Console.WriteLine("");
@@ -73,118 +135,7 @@ namespace ForestFireModel
             }
 
             forest.Step();
-            
-        }
-    }
 
-    public class ForestModel
-    {
-        public enum State : byte
-        {
-            Empty = 0,
-            Tree = 1,
-            Fire = 2
-        }
-
-        private State[,] _matrix;
-
-        public State[,] GetState()
-        {
-            return _matrix;
-        }
-
-        private static readonly Random rand = new Random();
-
-        private float _woodFireRisk { get; set; }
-
-        private float _probabilityFillingWithWood { get; set; }
-
-        public int HeightMap { get; set; }
-
-        public int WidthMap { get; set; }
-
-        protected ForestModel(float f, float p, int height, int width)
-        {
-            _woodFireRisk = f;
-            _probabilityFillingWithWood = p;
-            HeightMap = height;
-            WidthMap = width;
-
-            Initialize();
-        }
-
-        public static Result<ForestModel> Create(float f, float p, int height, int width)
-        {
-            var errors = new List<string>();
-
-            if (!(0 <= f && f <= 1)) errors.Add("Вероятность возгорания дерева только [0,1]");
-            if (!(0 <= p && p <= 1)) errors.Add("Вероятность заполнением дерева только [0,1]");
-            if (height < 1) errors.Add("Высота не может быть меньше единицы");
-            if (width < 1) errors.Add("Ширина не может быть меньше единицы");
-
-            if (errors.Any())
-            {
-                return Result<ForestModel>.Fail(errors);
-            }
-
-            return Result<ForestModel>.Success(new ForestModel(f, p, height, width));
-        }
-
-        private void Initialize()
-        {
-            _matrix = new State[HeightMap, WidthMap];
-            _matrix.Initialize();
-        }
-
-        public State[,] Step()
-        {
-            var newState = (State[,])_matrix.Clone();
-
-            int numRows = _matrix.GetLength(0);
-            int numCols = _matrix.GetLength(1);
-
-            for (int r = 1; r < numRows - 1; r++)
-            {
-                for (int c = 1; c < numCols - 1; c++)
-                {
-                    switch (_matrix[r, c])
-                    {
-                        case State.Empty:
-                            if (rand.NextDouble() < _probabilityFillingWithWood)
-                                newState[r, c] = State.Tree;
-                            break;
-
-                        case State.Tree:
-                            if (NeighborHasState(_matrix, r, c, State.Fire) || rand.NextDouble() < _woodFireRisk)
-                                newState[r, c] = State.Fire;
-                            break;
-
-                        case State.Fire:
-                            newState[r, c] = State.Empty;
-                            break;
-                    }
-                }
-            }
-
-            _matrix = newState;
-            return newState;
-        }
-
-        private bool NeighborHasState(State[,] state, int x, int y, State value)
-        {
-            for (int r = -1; r <= 1; r++)
-            {
-                for (int c = -1; c <= 1; c++)
-                {
-                    if (r == 0 && c == 0)
-                        continue;
-
-                    if (state[x + r, y + c] == value)
-                        return true;
-                }
-            }
-
-            return false;
         }
     }
 }
