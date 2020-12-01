44136b8d311dee51e010d3116869dd5a73ee527c
Evgeny Gorb <eugenegorb85@gmail.com>
2020-12-01 10:07:28 +0200

    No changes

--- AddressBook/AddressBookService/AddressBookServiceContract.cs
+++ AddressBook/AddressBookService/AddressBookServiceContract.cs
@@ -7,19 +7,15 @@ using System.ServiceModel;
 using System.ServiceModel.Web;
 using System.Text;
 
-namespace AddressBookService
-{
+namespace AddressBookService {
     // ПРИМЕЧАНИЕ. Команду "Переименовать" в меню "Рефакторинг" можно использовать для одновременного изменения имени класса "Service1" в коде и файле конфигурации.
-    public class AddressBookServiceContract : IAddressBookServiceContract
-    {
+    public class AddressBookServiceContract : IAddressBookServiceContract {
 
         #region User
         //[WebInvoke(Method = "POST", UriTemplate = "User/Add/{id}")]
-        public bool AddUser(string login, string password)
-        {
+        public bool AddUser(string login, string password) {
             var newUser = User.Register(login, password);
-            if (newUser != null)
-            {
+            if (newUser != null) {
                 var context = new AddressBookContext();
                 context.Users.Add(newUser.Value);
                 context.SaveChanges();
@@ -29,12 +25,10 @@ namespace AddressBookService
         }
 
         [WebInvoke(Method = "POST", UriTemplate = "User/Delete/{id}")]
-        public bool DeleteUser(int id)
-        {
+        public bool DeleteUser(int id) {
             var context = new AddressBookContext();
             var user = context.GetUser(id);
-            if (user != null)
-            {
+            if (user != null) {
                 context.Users.Remove(user);
                 context.SaveChanges();
                 return true;
@@ -46,12 +40,10 @@ namespace AddressBookService
 
         #region Subscriber
         [WebInvoke(Method = "POST", UriTemplate = "User/{id}/AddSubscriber")]
-        public bool AddSubscriber(int UserId, string firstName, string middleName, string lastName, DateTime? dateOfBirth, byte[] photo, Sex sex, string mail)
-        {
+        public bool AddSubscriber(int UserId, string firstName, string middleName, string lastName, DateTime? dateOfBirth, byte[] photo, Sex sex, string mail) {
             var context = new AddressBookContext();
             var user = context.GetUser(UserId);
-            if (user != null)
-            {               
+            if (user != null) {
                 user.AddSubscriber(firstName, middleName, lastName, dateOfBirth, photo, sex, mail);
                 context.SaveChanges();
                 return true;
@@ -61,15 +53,13 @@ namespace AddressBookService
         }
 
         [WebInvoke(Method = "POST", UriTemplate = "User/{UserId}/DeleteSubscriber/{id}")]
-        public bool DeleteSubscriber(int UserId, int id)
-        {
+        public bool DeleteSubscriber(int UserId, int id) {
             var context = new AddressBookContext();
             var user = context.GetUser(UserId);
-            if (user != null)
-            {
+            if (user != null) {
                 var subscriber = user.Subscribers.ToList().Find(s => s.Id == id);
                 if (subscriber != null)
-                user.RemoveSubscriber(subscriber);
+                    user.RemoveSubscriber(subscriber);
                 context.SaveChanges();
                 return true;
             }
@@ -78,15 +68,12 @@ namespace AddressBookService
         }
 
         [WebInvoke(Method = "GET", UriTemplate = "User/{UserId}/Subscriber/{id}")]
-        public SubscriberDto GetSubscriber(int UserId, int id)
-        {
+        public SubscriberDto GetSubscriber(int UserId, int id) {
             var context = new AddressBookContext();
             var user = context.GetUser(UserId);
-            if (user != null)
-            {
+            if (user != null) {
                 var subscriber = user.Subscribers.ToList().Find(s => s.Id == id);
-                if (subscriber != null)
-                {
+                if (subscriber != null) {
                     return SubscriberDto.Get(subscriber);
                 }
             }
@@ -96,12 +83,10 @@ namespace AddressBookService
 
         #region GroupAddress
         [WebInvoke(Method = "POST", UriTemplate = "User/{Userid}/AddGroupAddress")]
-        public bool AddGroupAddress(int UserId, string name)
-        {
+        public bool AddGroupAddress(int UserId, string name) {
             var context = new AddressBookContext();
             var user = context.GetUser(UserId);
-            if (user != null)
-            {
+            if (user != null) {
                 user.AddGroupAddress(name);
                 context.SaveChanges();
                 return true;
@@ -111,15 +96,13 @@ namespace AddressBookService
         }
 
         [WebInvoke(Method = "POST", UriTemplate = "User/{UserId}/DeleteGroupAddress/{id}")]
-        public bool DeleteGroupAddress(int UserId, int id)
-        {
+        public bool DeleteGroupAddress(int UserId, int id) {
             var context = new AddressBookContext();
             var user = context.GetUser(UserId);
-            if (user != null)
-            {
+            if (user != null) {
                 var groupAddress = user.GroupAddresses.ToList().Find(s => s.Id == id);
                 if (groupAddress != null)
-                   user.RemoveGroupAddress(groupAddress);
+                    user.RemoveGroupAddress(groupAddress);
                 context.SaveChanges();
                 return true;
             }
@@ -128,15 +111,12 @@ namespace AddressBookService
         }
 
         [WebInvoke(Method = "GET", UriTemplate = "User/{UserId}/GroupAddress/{id}")]
-        public GroupAddressDto GetGroupAddress(int UserId, int id)
-        {
+        public GroupAddressDto GetGroupAddress(int UserId, int id) {
             var context = new AddressBookContext();
             var user = context.GetUser(UserId);
-            if (user != null)
-            {
+            if (user != null) {
                 var groupAddress = user.GroupAddresses.ToList().Find(s => s.Id == id);
-                if (groupAddress != null)
-                {
+                if (groupAddress != null) {
                     return GroupAddressDto.Get(groupAddress);
                 }
             }
@@ -146,12 +126,10 @@ namespace AddressBookService
 
         #region Group
         [WebInvoke(Method = "POST", UriTemplate = "User/{Userid}/AddGroup")]
-        public bool AddGroup(int UserId, string name)
-        {
+        public bool AddGroup(int UserId, string name) {
             var context = new AddressBookContext();
             var user = context.GetUser(UserId);
-            if (user != null)
-            {
+            if (user != null) {
                 user.AddGroup(name);
                 context.SaveChanges();
                 return true;
@@ -161,12 +139,10 @@ namespace AddressBookService
         }
 
         [WebInvoke(Method = "POST", UriTemplate = "User/{UserId}/DeleteGroup/{id}")]
-        public bool DeleteGroup(int UserId, int id)
-        {
+        public bool DeleteGroup(int UserId, int id) {
             var context = new AddressBookContext();
             var user = context.GetUser(UserId);
-            if (user != null)
-            {
+            if (user != null) {
                 var group = user.Groups.ToList().Find(s => s.Id == id);
                 if (group != null)
                     user.RemoveGroup(group);
@@ -178,15 +154,12 @@ namespace AddressBookService
         }
 
         [WebInvoke(Method = "GET", UriTemplate = "User/{UserId}/Group/{id}")]
-        public GroupDto GetGroup(int UserId, int id)
-        {
+        public GroupDto GetGroup(int UserId, int id) {
             var context = new AddressBookContext();
             var user = context.GetUser(UserId);
-            if (user != null)
-            {
+            if (user != null) {
                 var group = user.Groups.ToList().Find(s => s.Id == id);
-                if (group != null)
-                {
+                if (group != null) {
                     return GroupDto.Get(group);
                 }
             }
@@ -196,12 +169,10 @@ namespace AddressBookService
 
         #region GroupPhone
         [WebInvoke(Method = "POST", UriTemplate = "User/{Userid}/AddGroupPhone")]
-        public bool AddGroupPhone(int UserId, string name)
-        {
+        public bool AddGroupPhone(int UserId, string name) {
             var context = new AddressBookContext();
             var user = context.GetUser(UserId);
-            if (user != null)
-            {
+            if (user != null) {
                 user.AddGroupPhone(name);
                 context.SaveChanges();
                 return true;
@@ -211,15 +182,12 @@ namespace AddressBookService
         }
 
         [WebInvoke(Method = "POST", UriTemplate = "User/{UserId}/DeleteGroupPhone/{id}")]
-        public bool DeleteGroupPhone(int UserId, int id)
-        {
+        public bool DeleteGroupPhone(int UserId, int id) {
             var context = new AddressBookContext();
             var user = context.GetUser(UserId);
-            if (user != null)
-            {
+            if (user != null) {
                 var groupPhone = user.GroupPhones.ToList().Find(s => s.Id == id);
-                if (groupPhone != null)
-                {
+                if (groupPhone != null) {
                     user.RemoveGroupPhone(groupPhone);
                     //context.GroupPhones.Remove(groupPhone);
                     context.SaveChanges();
@@ -232,15 +200,12 @@ namespace AddressBookService
         }
 
         [WebInvoke(Method = "GET", UriTemplate = "User/{UserId}/GroupPhone/{id}")]
-        public GroupPhoneDto GetGroupPhone(int UserId, int id)
-        {
+        public GroupPhoneDto GetGroupPhone(int UserId, int id) {
             var context = new AddressBookContext();
             var user = context.GetUser(UserId);
-            if (user != null)
-            {
+            if (user != null) {
                 var groupPhone = user.GroupPhones.ToList().Find(s => s.Id == id);
-                if (groupPhone != null)
-                {
+                if (groupPhone != null) {
                     return GroupPhoneDto.Get(groupPhone);
                 }
             }
